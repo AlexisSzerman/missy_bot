d7dae2828ced283e7fdd88f279eb7d5e42cb6019
Alexis Szerman <alexszer1986@gmail.com>
2024-06-04 16:46:56 -0300

    adding procfile 004

--- app.py
+++ app.py
@@ -175,7 +175,6 @@ async def resultado_busqueda(update: Update, context: ContextTypes.DEFAULT_TYPE)
         return ConversationHandler.END
 
 
-# Configuración del bot y manejadores de comandos
 if __name__ == '__main__':
     app = ApplicationBuilder().token(telegram_bot_api_key).build()
 
@@ -193,4 +192,8 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("siguiente", obtener_reuniones_siguiente))
     app.add_handler(buscar_handler)  # Agrega buscar_handler en lugar de resultado_busqueda
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_password))
-    app.run_polling()
+    
+    if os.getenv("TELEGRAM_BOT_API_KEY"):  # Verifica si la clave de la API de Telegram está presente en las variables de entorno
+        app.run_polling()
+    else:
+        print("La clave de la API de Telegram no está configurada en las variables de entorno.")
